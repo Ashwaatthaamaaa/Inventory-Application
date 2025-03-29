d4b6c0bef3cb011358d4f8a9dfaf68d953507a15
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-30 01:06:54 +0530

    Format connectionString in db/pool.js for consistency and readability

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
 module.exports = new Pool({
-    connectionString:"postgresql://lunge:291152@localhost:5432/inventory_db"
+    connectionString: "postgresql://lunge:291152@localhost:5432/inventory_db"
 });
