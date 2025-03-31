4cd6d216057b97243dd5b257128e52ef29d8801e
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-04-01 02:16:57 +0530

    Add root route to router.js for accessing the login page. This change allows users to reach the login page directly via the base URL.

--- routes/router.js
+++ routes/router.js
@@ -4,6 +4,7 @@ const { requireLogin } = require('../middleware/auth');
 const router = express.Router();
 
 // Public routes
+router.get('/',controller.getLoginPage)
 router.get('/login', controller.getLoginPage);
 router.post('/login', controller.handleLogin);
 router.get('/logout', controller.handleLogout);
